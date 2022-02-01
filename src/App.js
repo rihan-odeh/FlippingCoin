50ac1e4809c647bc316a8c2e6c15ff820c146c1a
Rihan <rihan.odeh.96@gmail.com>
2022-02-01 16:33:20 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FlippingCoin from './FlippingCoin';
+
+function App() {
+  return (
+    <div>
+<FlippingCoin/>
+    </div>
+  );
+}
+
+export default App;
